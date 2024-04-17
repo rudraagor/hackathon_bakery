c947533717dc8020c2eba3a16f036fe4251624c0
Rudraa gor <rudraagor@gmail.com>
2024-04-17 17:14:21 +0530

    db

---
+++ connection.php
@@ -0,0 +1,23 @@
+<?php
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    echo $username;
+
+    // database connection
+
+    $conn = new mysqli('localhost', 'root', '', 'bakery_db');
+
+    if($conn->connect_error){
+        die('Connection Failed : ' .$conn->connect_error);
+    }
+    else{
+        $stmt = $conn->prepare("insert into accountmn(username, email, pass) values(?, ?, ?)");
+
+        $stmt->bind_param("sss", $username, $email, $pass);
+        $stmt->execute();
+        echo"Successfully registered";
+        $stmt->close();
+        $conn->close();
+    }
+?>
